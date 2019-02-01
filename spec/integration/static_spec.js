937e4d81c4107af4cc248d2a0c945f9fad3f9a06
Soo Kim <chizbob@gmail.com>
2019-02-01 13:20:08 -0800

    AS2-Express completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,21 +3,26 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
-
-//#1
   describe("GET /", () => {
-
-//#2
     it("should return status code 200", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
         done();
       });
     });
-
   });
+  describe("GET /marco", ()=>{
+    it("should return status 200", (done) =>{
+      request.get('http://localhost:3000/marco', (err, res, body)=>{
+        expect(res.statusCode).toBe(200)
+        done()
+      })
+    })
+    it("should return Polo", (done)=>{
+      request.get('http://localhost:3000/marco', (err, res, body)=>{
+        expect(body).toContain('Polo')
+        done()
+      })
+    })
+  })
 });
