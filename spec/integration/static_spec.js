f204402a20d650a4e9f18ae6077c7b8ae9527028
Soo Kim <chizbob@gmail.com>
2019-02-02 10:16:23 -0800

    nothing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,17 +12,17 @@ describe("routes : static", () => {
     });
   });
   describe("GET /marco", ()=>{
-    it("should return status 200", (done) =>{
-      request.get('http://localhost:3000/marco', (err, res, body)=>{
-        expect(res.statusCode).toBe(200)
-        done()
+      it("should return status 200", (done) =>{
+        request.get('http://localhost:3000/marco', (err, res, body)=>{
+          expect(res.statusCode).toBe(200)
+          done()
+        })
+      })
+      it("should return Polo", (done)=>{
+        request.get('http://localhost:3000/marco', (err, res, body)=>{
+          expect(body).toContain('Polo')
+          done()
+        })
       })
     })
-    it("should return Polo", (done)=>{
-      request.get('http://localhost:3000/marco', (err, res, body)=>{
-        expect(body).toContain('Polo')
-        done()
-      })
-    })
-  })
 });
