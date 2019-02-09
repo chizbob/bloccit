f3b99e52ab6233c92ffc5c86583d25f78310d106
Soo Kim <chizbob@gmail.com>
2019-02-09 14:56:39 -0800

    pushed for coach review

--- spec/unit/user_spec.js
+++ spec/unit/user_spec.js
@@ -1,9 +1,9 @@
+const sequelize = require("../../src/db/models/index").sequelize;
 const User = require("../../src/db/models").User;
 
 describe("User", () => {
 
   beforeEach((done) => {
-// #1
     sequelize.sync({force: true})
     .then(() => {
       done();
@@ -17,7 +17,6 @@ describe("User", () => {
 
   describe("#create()", () => {
 
-// #2
     it("should create a User object with a valid email and password", (done) => {
       User.create({
         email: "user@example.com",
@@ -34,22 +33,15 @@ describe("User", () => {
       });
     });
 
-// #3
     it("should not create a user with invalid email or password", (done) => {
       User.create({
         email: "It's-a me, Mario!",
         password: "1234567890"
       })
       .then((user) => {
-
-        // The code in this block will not be evaluated since the validation error
-        // will skip it. Instead, we'll catch the error in the catch block below
-        // and set the expectations there.
-
         done();
       })
       .catch((err) => {
-// #4
         expect(err.message).toContain("Validation error: must be a valid email");
         done();
       });
@@ -57,7 +49,6 @@ describe("User", () => {
 
     it("should not create a user with an email already taken", (done) => {
 
-// #5
       User.create({
         email: "user@example.com",
         password: "1234567890"
@@ -69,11 +60,6 @@ describe("User", () => {
           password: "nananananananananananananananana BATMAN!"
         })
         .then((user) => {
-
-          // the code in this block will not be evaluated since the validation error
-          // will skip it. Instead, we'll catch the error in the catch block below
-          // and set the expectations there
-
           done();
         })
         .catch((err) => {
