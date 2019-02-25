5ce68c9519eeed0fff382b27ca9492b18ff12c4f
Soo Kim <chizbob@gmail.com>
2019-02-25 14:57:40 -0800

    CH13-comment-completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,15 +4,17 @@ module.exports = {
     const postRoutes = require("../routes/posts");
     const userRoutes = require("../routes/users");
     const topicRoutes = require("../routes/topics");
+    const commentRoutes = require("../routes/comments");
 
     if(process.env.NODE_ENV === "test") {
       const mockAuth = require("../../spec/support/mock-auth.js");
       mockAuth.fakeIt(app);
     }
-    
+
     app.use(staticRoutes);
     app.use(postRoutes);
     app.use(userRoutes);
     app.use(topicRoutes);
+    app.use(commentRoutes);    
   }
 }
