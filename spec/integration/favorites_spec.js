f03e88bb549263d4e88dcec1db3f1a220f395441
Soo Kim <chizbob@gmail.com>
2019-02-28 12:32:40 -0800

    typo resolved

--- spec/integration/favorites_spec.js
+++ spec/integration/favorites_spec.js
@@ -140,30 +140,32 @@ describe("routes : favorites", () => {
 
     describe("POST /topics/:topicId/posts/:postId/favorites/:id/destroy", () => {
 
-      it("should destroy a favorite", (done) => {
-        const options = {
-          url: `${base}${this.topic.id}/posts/${this.post.id}/favorites/create`
-        };
+          it("should destroy a favorite", (done) => {
+            const options = {
+              url: `${base}${this.topic.id}/posts/${this.post.id}/favorites/create`
+            };
 
-        let favCountBeforeDelete;
+            let favCountBeforeDelete;
 
-        request.post(options, (err, res, body) => {
-          this.post.getFavorites()
-          .then((favorites) => {
-            const favorite = favorites[0];
-            favCountBeforeDelete = favorites.length;
+            request.post(options, (err, res, body) => {
+              this.post.getFavorites()
+              .then((favorites) => {
+                console.log(favorites);
+                const favorite = favorites[0];
+                favCountBeforeDelete = favorites.length;
 
-            request.post(`${base}${this.topic.id}/posts/${this.post.id}/favorites/${favorites.id}/destroy`,
-              (err, res, body) => {
-                this.post.getFavorites()
-                .then((favorites) => {
-                  expect(favorites.length).toBe(favCountBeforeDelete - 1);
-                  done();
-                });
-              });
+                request.post(`${base}${this.topic.id}/posts/${this.post.id}/favorites/${favorite.id}/destroy`,
+                  (err, res, body) => {
+                    this.post.getFavorites()
+                    .then((favorites) => {
+                      console.log(this.post)
+                      expect(favorites.length).toBe(favCountBeforeDelete - 1);
+                      done();
+                    })
+                  })
+              })
+            })
           });
-        });
       });
-    });
   });
 });
